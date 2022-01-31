f8024aac9f0729e9cd2810446cddb2279fe69ef8
cepxat <sgungor@gmail.com>
2022-01-31 22:13:45 +0300

    Sudoku Solver Solution Update

--- Projects/1_Sudoku/solution.py
+++ Projects/1_Sudoku/solution.py
@@ -5,15 +5,29 @@ from utils import *
 row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+diagonal_units = [[rows[i] + cols[i] for i in range(9)], [rows[::-1][i] + cols[i] for i in range(9)]]
+
+# print('Diagonal Units')
+# print(diagonal_units)
 unitlist = row_units + column_units + square_units
 
 # TODO: Update the unit list to add the new diagonal units
-unitlist = unitlist
+unitlist = unitlist + diagonal_units
 
+# print('unitlist:')
+# print(unitlist)
 
 # Must be called after all units (including diagonals) are added to the unitlist
 units = extract_units(unitlist, boxes)
+
+# print('Units:')
+# print(units)
+
 peers = extract_peers(units, boxes)
+
+# print('Peers:')
+# print(peers)
+
 
 
 def naked_twins(values):
@@ -53,8 +67,45 @@ def naked_twins(values):
     Pseudocode for this algorithm on github:
     https://github.com/udacity/artificial-intelligence/blob/master/Projects/1_Sudoku/pseudocode.md
     """
-    # TODO: Implement this function!
-    raise NotImplementedError
+
+    # print('Naked Twins Start')
+    
+    nw_sudoku = values.copy()
+
+    for boxA in [a for a in values if len(values[a]) == 2]:
+        # print('boxA:')
+        # print(boxA)
+        # print(values[boxA])
+        for boxB in [b for b in peers[boxA] if values[boxA] == values[b]]:
+            # print(boxB)
+            # print(values[boxB])
+            # print('peers BoxA:')
+            # print(peers[boxA])
+            # print('peers BoxB:')
+            # print(peers[boxB])
+            for peerA in[p for p in peers[boxA]]:
+                # print('peerA')
+                # print(peerA)
+                for peerB in[q for q in peers[boxB]]:
+                    # print('peerB')
+                    # print(peerB)
+                    if peerA == peerB and len(values[peerA]) > 1:
+                        # print('Intersection peer')
+                        # print(peerA)
+                        # print(peerB)
+                        # print(values[peerA])
+                        for char in values[boxA]:
+                            # print('values peerA')
+                            # print(values[peerA])
+                            # nw_sudoku[peerA] = values[peerA].replace(char, '')
+                            nw_sudoku = assign_value(values, peerA, values[peerA].replace(char, ''))                            
+                            # print('Replaced char:')
+                            # print(char)
+                            # print('nw_sudoku PeerA')
+                            # print(nw_sudoku[peerA])
+
+    # print('Naked Twins End')
+    return nw_sudoku
 
 
 def eliminate(values):
@@ -73,8 +124,13 @@ def eliminate(values):
     dict
         The values dictionary with the assigned values eliminated from peers
     """
-    # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    for box in solved_values:
+        digit = values[box]
+        for peer in peers[box]:
+            values[peer] = values[peer].replace(digit,'')
+    return values
 
 
 def only_choice(values):
@@ -97,8 +153,13 @@ def only_choice(values):
     -----
     You should be able to complete this function by copying your code from the classroom
     """
-    # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+
+    for unit in unitlist:
+        for digit in '123456789':
+            dplaces = [box for box in unit if digit in values[box]]
+            if len(dplaces) == 1:
+                values[dplaces[0]] = digit
+    return values
 
 
 def reduce_puzzle(values):
@@ -115,8 +176,19 @@ def reduce_puzzle(values):
         The values dictionary after continued application of the constraint strategies
         no longer produces any changes, or False if the puzzle is unsolvable 
     """
-    # TODO: Copy your code from the classroom and modify it to complete this function
-    raise NotImplementedError
+
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    stalled = False
+    while not stalled:
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        values = eliminate(values)
+        values = only_choice(values)
+        values = naked_twins(values)
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        stalled = solved_values_before == solved_values_after
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
 
 
 def search(values):
@@ -138,8 +210,28 @@ def search(values):
     You should be able to complete this function by copying your code from the classroom
     and extending it to call the naked twins strategy.
     """
-    # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+
+
+    # First, reduce the puzzle using the previous function
+    values = reduce_puzzle(values)
+    if values is False:
+        return False ## Failed earlier
+    if all(len(values[s]) == 1 for s in boxes): 
+        return values ## Solved!
+    # Choose one of the unfilled squares with the fewest possibilities
+    n,s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+
+    # print(n)
+    # print(s)
+
+    # display(values)
+    # Now use recurrence to solve each one of the resulting sudokus, and 
+    for value in values[s]:
+        new_sudoku = values.copy()
+        new_sudoku[s] = value
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
 
 
 def solve(grid):
